c3e664f3ff8e74ff417606bb0aa93573c26c384c
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-09-05 17:06:18 +0100

    Update layout.tsx
    Reduce the fonts weights and font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,13 @@
 import type { Metadata } from "next";
-import { Play, Poppins, Open_Sans } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const play = Play({
-  variable: "--font-play-next",
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
-
 const poppins = Poppins({
   variable: "--font-poppins-next",
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
-
-const openSans = Open_Sans({
-  variable: "--font-open-sans-next",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -37,13 +25,13 @@ export default function RootLayout({
     <html
       lang="en"
       data-scroll-behavior="smooth"
-      className={`${play.variable} ${poppins.variable} ${openSans.variable}`}
+      className={poppins.variable}
     >
       <body>
         <Header />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
